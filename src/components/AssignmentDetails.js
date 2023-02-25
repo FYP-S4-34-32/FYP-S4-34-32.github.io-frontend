50bae79d69421e6d356e8e183fc17ac587c5773c
mattyjy <104262448+mattyjy@users.noreply.github.com>
2023-02-25 10:50:19 +0800

    as

--- src/components/AssignmentDetails.js
+++ src/components/AssignmentDetails.js
@@ -525,6 +525,7 @@ const AssignmentDetails = () => {
             error ? <p>{error}</p> : <Bar key={uniqueKey} data={showData()} /> :
             null
             }
+            <p>Test</p>
             </div>
         );
     }
